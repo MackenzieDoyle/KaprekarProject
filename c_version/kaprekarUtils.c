07f20e3cf2a208b46a45b21a68d470ad16afc6b9
MackenzieDoyle <mackenziedoyle@huskers.unl.edu>
2019-09-13 10:33:58 -0500

    Fixed spelling error in variable name
    On line 43, I changed the incorrectly spelled "modulous" to "modulus", a variable that has been previously declared.

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -40,7 +40,7 @@
     //for each possible "split" of the square...
     for(i=1; i<=numDigits; i++) {
       //increase the modulus by a factor of 10
-      modulous *= 10;
+      modulus *= 10;
 
       //split the square into two parts
       first = square / modulus;
