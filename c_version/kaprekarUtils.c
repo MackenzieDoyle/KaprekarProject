2d79dedaa1adeac98057974be1cd3d5b675a57bd
MackenzieDoyle <mackenziedoyle@huskers.unl.edu>
2019-09-13 10:44:58 -0500

    Fixed loop errors
    Changed long modulus to 1 instead of zero, so that when it multiplies itself by ten in the loop, it is not constantly outputting zero.
    Also changed the log10(n) on line 36 to log10(square) since the comment says to compute the number of digits of the square, not of n.

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -33,8 +33,8 @@
     //compute the square
     long square = n * (long) n;
     //compute the number of digits of the square
-    int numDigits = (int) log10(n) + 1;
-    long modulus = 0;
+    int numDigits = (int) log10(square) + 1;
+    long modulus = 1;
     long first, second;
 
     //for each possible "split" of the square...
